3fa3cc594ba4beb9ed65d3a519a0027a15555526
nathan <px61pje@eduvaud.ch>
2026-03-09 21:31:49 +0100

    update (code) ajout de malus et de bonus

---
+++ KnigtsOdyssey_game/scenes/pickup_s.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# Type de l'effet : "speed_boost", "jump_boost", ou "speed_malus"
+@export_enum("speed_boost", "jump_boost", "speed_malus") var effect_type: String = "jump_boost"
+
+# Durée de l'effet en secondes
+@export var duration: float = 3.0
+
+func _on_body_entered(body: Node2D) -> void:
+	print("quelque chose a touché le pickup : ", body.name)
+	if body.is_in_group("Player"):
+		print("c'est le joueur !")
+		body.apply_effect(effect_type, duration)
+		queue_free()
